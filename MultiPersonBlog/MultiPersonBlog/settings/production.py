4fcb7331be6e8e3a49a69e3f225f49c02952fd5c
wquanyuan <quanwyuan@gmail.com>
2020-06-15 21:52:19 -0700

    initialize

---
+++ MultiPersonBlog/MultiPersonBlog/settings/production.py
@@ -0,0 +1,27 @@
+# flake8: NOQA
+
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'CONN_MAX_AGE': 60,
+        'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+"""
+
